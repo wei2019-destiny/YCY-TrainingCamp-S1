1367d7cff5ca0f2d0eac8189db1bd4fc1ed19e28
wei2019-destiny <1286780926@qq.com>
2022-07-28 17:32:33 +0800

    刷漆

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -12,22 +12,20 @@ class BrakeBanner {
 		this.stage = this.app.stage;
 		this.souceArray = ['btn.png', 'btn_circle.png', 'brake_bike.png', 'brake_handlerbar.png', 'brake_lever.png', 'malu.png']
 		this.addSouce('images');
-		this.loader.load();
+		this.loader.load();//加载元素
 
 		this.bikelever = null;//预留一个刹车的对象
 
 		this.loader.onComplete.add(() => {
 			this.show();
 		})
-
 	}
-
-	addSouce(basePath, souceArray = this.souceArray) {
+	addSouce(basePath, souceArray = this.souceArray) {//添加资源
 		for (const name of souceArray) {
 			this.loader.add(name, `${basePath}/${name}`);
 		}
 	}
-	show() {
+	show() {//显示
 		let road = this.roadContainer()
 		let bike = this.createBike();
 		let particles = this.createParticleContainer()
@@ -38,13 +36,13 @@ class BrakeBanner {
 		actionButton.interactive = true;
 		actionButton.buttonMode = true;//更改cursor 亦可借助actionButton.cursor = 'wait';更改
 
-		actionButton.on('pointerdown', (e) => {
+		actionButton.on('pointerdown', (e) => {//桌面端移动端兼容的鼠标按下事件
 			gsap.to(this.bikelever, { duration: 0.4, rotation: Math.PI / 180 * -35, repeat: 0 });
 			gsap.to(bike, { duration: 0.3, x: bike.x - 20, y: bike.y + 20, repeat: 0 });
 			particles.pause();
 			road.pause();
 		})
-		actionButton.on('pointerup', (e) => {
+		actionButton.on('pointerup', (e) => {//桌面端移动端兼容的鼠标抬起事件
 			gsap.to(this.bikelever, { duration: 0.4, rotation: 0, repeat: 0 });
 			gsap.to(bike, { duration: 0.3, x: bike.x + 20, y: bike.y - 20, repeat: 0 });
 			particles.start();
@@ -52,15 +50,16 @@ class BrakeBanner {
 		})
 
 		let resize = () => {
-			bike.x = window.innerWidth - bike.width + 20;
+			bike.x = window.innerWidth - bike.width + 20;//使窗口变化后自行车依然保持在右侧
 			bike.y = window.innerHeight - bike.height;
 		}
 		window.addEventListener('resize', resize)
 		resize();
 	}
-	getResources(name) {
+	getResources(name) {//获得元素内容
 		return new PIXI.Sprite(this.loader.resources[name].texture);
 	}
+
 	createActionButton() {
 		let actionButton = new PIXI.Container();//初始化一个容器
 		this.stage.addChild(actionButton);//将容器加载到stage中
@@ -95,6 +94,9 @@ class BrakeBanner {
 		let bikeHandbar = this.getResources('brake_handlerbar.png');
 		let bikelever = this.getResources('brake_lever.png');
 		this.bikelever = bikelever;//刹车挂载到this上
+		bikeimage.tint = 0x05AFF2; // 给自行车着色 需要注意的是，着色是在原色彩基础上混合颜色而不是覆盖颜色
+		bikeHandbar.tint = 0x94E1F2; 
+		bikelever.tint = 0x0442BF; 
 
 		bikelever.pivot.x = bikelever.pivot.y = 455;
 		bikelever.x = 772;
